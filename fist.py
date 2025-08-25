0bb99bd65ba7e0a3d1b2e8b9d27ec1138306d812
Shivam Bhardwaj <bhardwajshivam358@gmail.com>
2025-08-25 12:41:47 +0530

    Add files via upload

---
+++ fist.py
@@ -0,0 +1,22 @@
+print("HELLO GUYS CALCULATOR USE KARLO!")
+num1 = float(input("PEHLA NUMBER DAAL:"))
+num2 = float(input("DUSRA BHI DAAL:"))
+op = input("KYA KARVANA HAI? (+, -, *, /):")
+if op == "+":
+    print("ISKA JAVAAB HAI ",num1 + num2)
+elif op == "-":
+    print("ISKA JAVAAB HAI ",num1 - num2)
+elif op == "*":
+    print("ISKA JAVAAB HAI ",num1 * num2)
+elif op == "/":
+    print("ISKA JAVAAB HAI ",num1 / num2)
+else:
+    print("MATLAB KUCH BHI.")
+print("DHANYAVAAD MERA USE KARNE KE LIYE, MWAH")
+print("BYE BYE BRO!")
+# This is a simple calculator program
+# It takes two numbers and an operation as input and outputs the result
+# Supported operations are +, -, *, /
+# If the user enters an invalid operation, it outputs an error message
+# The program ends with a goodbye message.
+# Enjoy using the calculator, you silly lil bih!
